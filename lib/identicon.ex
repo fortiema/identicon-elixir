b84f9062781033b320aca3fde0b2353be253d539
Matt <fortiema@gmail.com>
2018-07-08 11:56:59 -0400

    Add square filtering logic

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -7,6 +7,7 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_squares
   end
 
   @doc """
@@ -17,6 +18,7 @@ defmodule Identicon do
       iex> Identicon.hash_input("banana")
       %Identicon.Image{
         color: nil,
+        grid: nil,
         hex: [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]
       }
   """
@@ -29,18 +31,71 @@ defmodule Identicon do
 
   @doc """
     Updates an `Identicon.Image` with RGB values picked from its `hex` property.
+
+  ## Examples
+
+      iex> image = Identicon.hash_input("banana")
+      iex> Identicon.pick_color(image)
+      %Identicon.Image{
+          color: {114, 179, 2},
+          grid: nil,
+          hex: [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]
+        }
   """
   def pick_color(image) do
-    %Identicon.Image{hex: [r, g, b | _tail]} = image
+    %Identicon.Image{hex: [r, g, b | _]} = image
     %Identicon.Image{image | color: {r, g, b}}
   end
 
+  @doc """
+    Updates an `Identicon.Image` to construct its `grid` property based on its `hex` property.
 
+  ## Examples
+
+      iex> image = Identicon.hash_input("banana")
+      iex> Identicon.build_grid(image)
+      %Identicon.Image{
+          color: nil,
+          grid: [
+            {114, 0},
+            {179, 1},
+            {2, 2},
+            {179, 3},
+            {114, 4},
+            {191, 5},
+            {41, 6},
+            {122, 7},
+            {41, 8},
+            {191, 9},
+            {34, 10},
+            {138, 11},
+            {117, 12},
+            {138, 13},
+            {34, 14},
+            {115, 15},
+            {1, 16},
+            {35, 17},
+            {1, 18},
+            {115, 19},
+            {239, 20},
+            {239, 21},
+            {124, 22},
+            {239, 23},
+            {239, 24}
+          ],
+          hex: [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]
+        }
+  """
   def build_grid(image) do
-    %Identicon.Image{hex: hex_list} = image
-    hex_list
-    |> Enum.chunk(3)
-    |> Enum.map(&mirror_row/1)
+    %Identicon.Image{hex: hex} = image
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
   end
 
   @doc """
@@ -52,8 +107,17 @@ defmodule Identicon do
       [1, 2, 3, 2, 1]
   """
   def mirror_row(row) do
-    [first, second | _tail] = row
+    [first, second | _] = row
     row ++ [second, first]
+  end
+
+  def filter_squares(image) do
+    %Identicon.Image{grid: grid} = image
+    grid = Enum.filter grid, fn({val, _}) ->
+      rem(val, 2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
   end
 
 end
